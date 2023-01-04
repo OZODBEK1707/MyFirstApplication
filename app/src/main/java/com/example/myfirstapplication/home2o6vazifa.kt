be9021968e797ca267253679ca862356b419022f
user <rahmatullayev845@gmail.com>
2023-01-04 16:05:54 +0500

    FirstApplication

---
+++ app/src/main/java/com/example/myfirstapplication/home2o6vazifa.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home2o6vazifa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home2o6vazifa)
+    }
+}
